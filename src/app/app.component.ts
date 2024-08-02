1bdaa88e1e0b22e9f9a50c6c20ea190f29d6c682
Leonardo Freiria <88349331+LeonardoFreiria@users.noreply.github.com>
2024-08-02 16:34:38 -0300

    feat: Adicionar estilo de teste navbar e adicionar componente do navbar na raíz da aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from "@angular/core";
+import {RouterOutlet} from "@angular/router";
+import {NavbarComponent} from "./navbar/navbar.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'CineLog-Plus';
+  title = "CineLog-Plus";
 }
