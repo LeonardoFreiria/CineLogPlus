f9eb0928ff7727bb52d37c1b776d58f63dc1441e
Leonardo Freiria <88349331+LeonardoFreiria@users.noreply.github.com>
2024-08-04 16:52:25 -0300

    feat(barra-de-navegacao): Renomear pasta e componente de navbar para barra-de-navegacao

---
+++ src/app/barra-de-navegacao/barra-de-navegacao.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "app-barra-de-navegacao",
+  standalone: true,
+  imports: [],
+  templateUrl: "./barra-de-navegacao.component.html",
+  styleUrl: "./barra-de-navegacao.component.scss",
+})
+export class BarraDeNavegacaoComponent {}
